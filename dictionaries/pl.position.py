cd29462b099b9e0c78c626cfd81c2df25c2c1536
Herz-A <antonia.herz82166@gmail.com>
2025-05-27 13:26:30 +0200

    kartenposition als dictionary

---
+++ dictionaries/pl.position.py
@@ -0,0 +1,11 @@
+import settings as s
+
+eins = {'x': s.WIDTH // 16, "y": s.HEIGHT // 16, 'width': s.WIDTH // 4, 'height': s.HEIGHT // 4}
+zwei = {'x': 2 * s.WIDTH // 16 + s.WIDTH // 4, 'y': s.HEIGHT // 16, 'width': s.WIDTH // 4, 'height': s.HEIGHT // 4}
+drei = {'x': s.WIDTH * 3 // 4 + 2 * s.WIDTH // 4, 'y': s.HEIGHT // 16, 'width': s.WIDTH // 4, 'height': s.HEIGHT // 4}
+vier = {'x': s.WIDTH // 16, 'y': s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16, 'width': s.WIDTH // 4, 'height': s.HEIGHT // 4}
+
+fünf = {'x': 2 * s.WIDTH // 16 + s.WIDTH // 4, 'y': s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16, 'width': s.WIDTH // 4, 'height': s.HEIGHT // 4}
+sechs = {'x': s.WIDTH * 3 // 4 + 2 * s.WIDTH // 4, 'y': s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16, 'width': s.WIDTH // 4, 'height': s.HEIGHT // 4}
+
+carddeck = {'x': (s.WIDTH - (2*s.WIDTH // 16 + s.WIDTH // 4)) // 2, 'y': s.HEIGHT // 2 - (s.HEIGHT // 4) // 2, 'width': 2 * s.WIDTH // 16 + s.WIDTH // 4, 'height': s.HEIGHT // 4}
--- surface.py
+++ surface.py
@@ -17,17 +17,4 @@ def draw(player_name):
     player_text = s.PLAYER_FONT.render(player_name, 1, s.PLAYER_FONT_COLOR)
     s.WINDOW.blit(player_text, (s.PLAYER_X_POSITION, s.PLAYER_Y_POSITION))
 
-    # Obere Zeile Rechtecke
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 16, s.HEIGHT // 16, s.WIDTH // 4, s.HEIGHT // 4))
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16, s.HEIGHT // 16, s.WIDTH // 4, s.HEIGHT // 4))
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16, s.HEIGHT // 16, s.WIDTH // 4, s.HEIGHT // 4))
-
-    # Untere Zeile Rechtecke
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 16, s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16, s.WIDTH // 4, s.HEIGHT // 4))
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16, s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16, s.WIDTH // 4, s.HEIGHT // 4))
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16, s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16, s.WIDTH // 4, s.HEIGHT // 4))
-
-    # Karteneck, Nachziehstapel, Ablagestapel
-    pygame.draw.rect(s.WINDOW, s.BLACK, (s.WIDTH // 2 - (s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16) // 2, s.HEIGHT // 2 - (s.HEIGHT // 4) // 2, s.WIDTH // 16 + s.WIDTH // 4 + s.WIDTH // 16, s.HEIGHT // 4))
-
     pygame.display.flip()
