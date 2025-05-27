f406fbada56c1602dfcd2bc8e2dbf9bb39b1640d
Herz-A <antonia.herz82166@gmail.com>
2025-05-27 10:50:56 +0200

    surface with player name

--- surface.py
+++ surface.py
@@ -9,3 +9,12 @@ def first_draw():
     s.WINDOW.fill(s.WINDOW_COLOR)
 
     pygame.display.flip()
+
+
+def draw(player_name):
+    s.WINDOW.fill(s.WINDOW_COLOR)
+
+    player_text = s.PLAYER_FONT.render(player_name, 1, s.PLAYER_FONT_COLOR)
+    s.WINDOW.blit(player_text, (s.PLAYER_X_POSITION, s.PLAYER_Y_POSITION))
+
+    pygame.display.flip()
