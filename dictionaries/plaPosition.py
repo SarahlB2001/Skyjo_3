978d47e14b71a1be06ba1ac16b7c893c9231b2c9
Herz-A <antonia.herz82166@gmail.com>
2025-06-03 10:27:06 +0200

    Platzsets werden angezeigt (in Schwarz)

---
+++ dictionaries/plaPosition.py
@@ -0,0 +1,42 @@
+import settings as s
+
+# Für Spiel mit 2 Leuten: 1 & 3 (oder 2 & 5)
+# Für Spiel mit 3 Leuten: 1 & 3 & 5
+# Für Spiel mit 4 Leuten: 1 & 3 & 4 & 6
+# Für Spiel mit 5 Leuten: 1 & 2 & 3 & 4 & 6
+# Für Spiel mit 6 Leuten : 1 & 2 & 3 & 4 & 5 & 6
+
+eins = {'x': s.WIDTH // 16,
+        "y": s.HEIGHT // 16}
+
+zwei = {'x': s.WIDTH * 2 // 16 + s.WIDTH // 4,
+        'y': s.HEIGHT // 16}
+
+drei = {'x': s.WIDTH * 3 // 4 - s.WIDTH // 16,
+        'y': s.HEIGHT // 16}
+
+vier = {'x': s.WIDTH * 3 // 4 - s.WIDTH // 16,
+        'y': s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16}
+
+fünf = {'x': s.WIDTH // 8 + s.WIDTH // 4,
+        'y': s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16}
+
+sechs = {'x': s.WIDTH // 16,
+         'y': s.HEIGHT - s.HEIGHT//4 - s.HEIGHT//16}
+
+size = {'width': s.WIDTH // 4,
+        'height': s.HEIGHT // 4}
+
+carddeck = {'x': (s.WIDTH - (2*s.WIDTH // 16 + s.WIDTH // 4)) // 2,
+            'y': s.HEIGHT // 2 - (s.HEIGHT // 4) // 2,
+            'width': 2 * s.WIDTH // 16 + s.WIDTH // 4,
+            'height': s.HEIGHT // 4}
+
+field_pos = {'1': eins,
+             '2': zwei,
+             '3': drei,
+             '4': vier,
+             '5': fünf,
+             '6': sechs,
+             'size': size,
+             'carddeck': carddeck}
--- surface.py
+++ surface.py
@@ -1,4 +1,5 @@
 import settings as s
+from dictionaries import plaPosition as pl
 # from dictionaries import Spieler_Spielername as pln
 # import server as serv
 import pygame
@@ -13,6 +14,34 @@ def first_draw():
 
 def draw(player_name):
     s.WINDOW.fill(s.WINDOW_COLOR)
+
+    if s.PL_ANZAHL == 2:
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+    elif s.PL_ANZAHL == 3:
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['5'] ['x'], pl.field_pos['5'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+    elif s.PL_ANZAHL == 4:
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['4'] ['x'], pl.field_pos['4'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['6'] ['x'], pl.field_pos['6'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+    elif s.PL_ANZAHL == 5:
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['4'] ['x'], pl.field_pos['4'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['6'] ['x'], pl.field_pos['6'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['5'] ['x'], pl.field_pos['5'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+    elif s.PL_ANZAHL == 6:
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['2'] ['x'], pl.field_pos['2'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['4'] ['x'], pl.field_pos['4'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['6'] ['x'], pl.field_pos['6'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+        pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['5'] ['x'], pl.field_pos['5'] ['y'], pl.field_pos['size'] ['width'], pl.field_pos['size'] ['height']))
+
+    pygame.draw.rect(s.WINDOW, s.BLACK, (pl.field_pos['carddeck'] ['x'], pl.field_pos['carddeck'] ['y'], pl.field_pos['carddeck'] ['width'], pl.field_pos['carddeck'] ['height']))
 
     player_text = s.PLAYER_FONT.render(player_name, 1, s.PLAYER_FONT_COLOR)
     s.WINDOW.blit(player_text, (s.PLAYER_X_POSITION, s.PLAYER_Y_POSITION))
