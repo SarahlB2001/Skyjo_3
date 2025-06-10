b620fc69e5c7a926ef732ae287b29be733a751fc
MaritS23 <maritschiller23@gmail.com>
2025-06-10 14:23:30 +0200

    Bilschirmgröße berechnen

--- layout.py
+++ layout.py
@@ -2,10 +2,29 @@ from entities.cards import Card
 import settings as s
 import random as r
 import surface as sf
+import tkinter as tk 
+
+def get_screen_resolution():
+    root = tk.Tk()
+    root.withdraw()
+
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+
+    root.destroy() # Zerstört das Root-Fenster nach dem Abrufen der Auflösung
+    
+    return screen_width, screen_height 
+
+if __name__ == "__main__":
+    width, height = get_screen_resolution()
+    print(f"Bildschirmbreite: {width} Pixel")
+    print(f"Bildschirmhöhe: {height} Pixel")
 
 
 class CardLayout:
     def __init__( self, start_x, start_y, rows=s.ROWS, cols=s.COLS):
+
         self.cards = []
         for row in range (rows):
             row_cards = []
