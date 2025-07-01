8b6e61e5d531d51ebd8d6c50b6463fe1c2527471
Diedan Karim <diedan.karim@gmail.com>
2025-07-01 11:10:25 +0200

    bug fixing

--- dictionaries/cardSetPosition.py
+++ dictionaries/cardSetPosition.py
@@ -4,6 +4,8 @@ from dictionaries import plaPosition as pl
 import pygame
 import layout as l
 
+player_cardlayouts = {}
+
 # Position von den Plätzen auf dem Spielfeld
 
 def card_place_position():
@@ -35,31 +37,38 @@ def card_place_position():
 
     pygame.draw.rect(su.WINDOW, s.BLACK, (pl.field_pos['carddeck'] ['x'], pl.field_pos['carddeck'] ['y'], pl.field_pos['carddeck'] ['width'], pl.field_pos['carddeck'] ['height']))
 
+
 def card_set_positions():
+    global player_cardlayouts
+    player_cardlayouts = {}  # immer neu anlegen
+
     if s.PL_ANZAHL == 2:
-        player1_CardLayout = l.CardLayout(pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'])
-        player2_CardLayout = l.CardLayout(pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'])
-    if s.PL_ANZAHL == 3:
-        player1_CardLayout = l.CardLayout(pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'])
-        player2_CardLayout = l.CardLayout(pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'])
-        player3_CardLayout = l.CardLayout(pl.field_pos['5'] ['x'], pl.field_pos['5'] ['y'])
+        player_cardlayouts[1] = l.CardLayout(pl.field_pos['1']['x'], pl.field_pos['1']['y'])
+        player_cardlayouts[2] = l.CardLayout(pl.field_pos['3']['x'], pl.field_pos['3']['y'])
+    elif s.PL_ANZAHL == 3:
+        player_cardlayouts[1] = l.CardLayout(pl.field_pos['1']['x'], pl.field_pos['1']['y'])
+        player_cardlayouts[2] = l.CardLayout(pl.field_pos['3']['x'], pl.field_pos['3']['y'])
+        player_cardlayouts[3] = l.CardLayout(pl.field_pos['5']['x'], pl.field_pos['5']['y'])
     elif s.PL_ANZAHL == 4:
-        player1_CardLayout = l.CardLayout(pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'])
-        player2_CardLayout = l.CardLayout(pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'])
-        player3_CardLayout = l.CardLayout(pl.field_pos['4'] ['x'], pl.field_pos['4'] ['y'])
-        player4_CardLayout = l.CardLayout(pl.field_pos['6'] ['x'], pl.field_pos['6'] ['y'])
+        player_cardlayouts[1] = l.CardLayout(pl.field_pos['1']['x'], pl.field_pos['1']['y'])
+        player_cardlayouts[2] = l.CardLayout(pl.field_pos['3']['x'], pl.field_pos['3']['y'])
+        player_cardlayouts[3] = l.CardLayout(pl.field_pos['4']['x'], pl.field_pos['4']['y'])
+        player_cardlayouts[4] = l.CardLayout(pl.field_pos['6']['x'], pl.field_pos['6']['y'])
     elif s.PL_ANZAHL == 5:
-        player1_CardLayout = l.CardLayout(pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'])
-        player2_CardLayout = l.CardLayout(pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'])
-        player3_CardLayout = l.CardLayout(pl.field_pos['4'] ['x'], pl.field_pos['4'] ['y'])
-        player4_CardLayout = l.CardLayout(pl.field_pos['5'] ['x'], pl.field_pos['5'] ['y'])
-        player5_CardLayout = l.CardLayout(pl.field_pos['6'] ['x'], pl.field_pos['6'] ['y'])
+        player_cardlayouts[1] = l.CardLayout(pl.field_pos['1']['x'], pl.field_pos['1']['y'])
+        player_cardlayouts[2] = l.CardLayout(pl.field_pos['3']['x'], pl.field_pos['3']['y'])
+        player_cardlayouts[3] = l.CardLayout(pl.field_pos['4']['x'], pl.field_pos['4']['y'])
+        player_cardlayouts[4] = l.CardLayout(pl.field_pos['5']['x'], pl.field_pos['5']['y'])
+        player_cardlayouts[5] = l.CardLayout(pl.field_pos['6']['x'], pl.field_pos['6']['y'])
     elif s.PL_ANZAHL == 6:
-        player1_CardLayout = l.CardLayout(pl.field_pos['1'] ['x'], pl.field_pos['1'] ['y'])
-        player2_CardLayout = l.CardLayout(pl.field_pos['2'] ['x'], pl.field_pos['2'] ['y'])
-        player3_CardLayout = l.CardLayout(pl.field_pos['3'] ['x'], pl.field_pos['3'] ['y'])
-        player4_CardLayout = l.CardLayout(pl.field_pos['4'] ['x'], pl.field_pos['4'] ['y'])
-        player5_CardLayout = l.CardLayout(pl.field_pos['6'] ['x'], pl.field_pos['6'] ['y'])
-        player6_CardLayout = l.CardLayout(pl.field_pos['5'] ['x'], pl.field_pos['5'] ['y'])
+        player_cardlayouts[1] = l.CardLayout(pl.field_pos['1']['x'], pl.field_pos['1']['y'])
+        player_cardlayouts[2] = l.CardLayout(pl.field_pos['2']['x'], pl.field_pos['2']['y'])
+        player_cardlayouts[3] = l.CardLayout(pl.field_pos['3']['x'], pl.field_pos['3']['y'])
+        player_cardlayouts[4] = l.CardLayout(pl.field_pos['4']['x'], pl.field_pos['4']['y'])
+        player_cardlayouts[5] = l.CardLayout(pl.field_pos['6']['x'], pl.field_pos['6']['y'])
+        player_cardlayouts[6] = l.CardLayout(pl.field_pos['5']['x'], pl.field_pos['5']['y'])
 
     pygame.draw.rect(su.WINDOW, s.BLACK, (pl.field_pos['carddeck'] ['x'], pl.field_pos['carddeck'] ['y'], pl.field_pos['carddeck'] ['width'], pl.field_pos['carddeck'] ['height']))
+
+    for layout in player_cardlayouts.values():
+        layout.draw(su.WINDOW)
